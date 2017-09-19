a5a3fba7519c27c671f506a0096d007c1c0e8701
Swin.Liu <swin.liu@gpayroll.com>
2017-09-19 11:38:59 +0800

    add sh1 & rsa encrypt/decrypt test
    Change-Id: Ica3271bb265fd7228350f982d54aa1731b224c55

--- sFlow-frontend/src/app/components/user-management/user-add/user-add.component.ts
+++ sFlow-frontend/src/app/components/user-management/user-add/user-add.component.ts
@@ -2,6 +2,8 @@ import {Location} from '@angular/common';
 import {Component, NgModule, OnInit} from '@angular/core';
 import * as crypto from 'crypto-browserify';
 
+declare const Buffer;
+
 @Component({
   selector: 'sFlow-user-add',
   templateUrl: './user-add.component.html'
@@ -14,29 +16,26 @@ export class UserAddComponent implements OnInit {
     const sha1Str: string = crypto.createHash('sha1').update('test').digest('hex');
     console.log("sha1 = %s", sha1Str);
 
-    // const RSA_PUBLIC_KEY: string = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBi\n' +
-    //   'QKBgQCoTmibtJI3k06/qq7AtGBwMRHTYN0Wdsb0ff4024OBO/2KFYamBT5oiOUNxJSC5+GS+1u5S\n' +
-    //   'vRpdZV4NPsxAEF1gR/01BWD4ajQv0ZRrl6dmq9G2iUaGnTjC+0CzfdHj8SKoaP08B4frTBf+30+7m81KO7STli\n' +
-    //   'KijXXdHhh81mMVQIDAQAB';
+    const RSA_PUBLIC_KEY: string = '-----BEGIN PUBLIC KEY-----\n' +
+    'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAOkvtlnW0AZsuC4PCiaagn4tvCKxgHDi\n' +
+    'ttSM+3eOqmff7uCNb4A5MBpq6jJq2L7giQisp0ko1s1l4zchHAjN+fMCAwEAAQ==\n' +
+    '-----END PUBLIC KEY-----';
 
-    // var key = new NodeRSA('-----BEGIN RSA PRIVATE KEY-----\n'+
-    // 'MIIBOQIBAAJAVY6quuzCwyOWzymJ7C4zXjeV/232wt2ZgJZ1kHzjI73wnhQ3WQcL\n'+
-    // 'DFCSoi2lPUW8/zspk0qWvPdtp6Jg5Lu7hwIDAQABAkBEws9mQahZ6r1mq2zEm3D/\n'+
-    // 'VM9BpV//xtd6p/G+eRCYBT2qshGx42ucdgZCYJptFoW+HEx/jtzWe74yK6jGIkWJ\n'+
-    // 'AiEAoNAMsPqwWwTyjDZCo9iKvfIQvd3MWnmtFmjiHoPtjx0CIQCIMypAEEkZuQUi\n'+
-    // 'pMoreJrOlLJWdc0bfhzNAJjxsTv/8wIgQG0ZqI3GubBxu9rBOAM5EoA4VNjXVigJ\n'+
-    // 'QEEk1jTkp8ECIQCHhsoq90mWM/p9L5cQzLDWkTYoPI49Ji+Iemi2T5MRqwIgQl07\n'+
-    // 'Es+KCn25OKXR/FJ5fu6A6A+MptABL3r8SEjlpLc=\n'+
-    // '-----END RSA PRIVATE KEY-----');
-    // const encrypted1 = crypto.publicEncrypt(RSA_PUBLIC_KEY, "test").toString('hex');
-    // console.log("RSA = %s", encrypted1);
+    const RSA_PRIVATE_KEY = '-----BEGIN PRIVATE KEY-----\n' +
+    'MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEA6S+2WdbQBmy4Lg8K\n' +
+    'JpqCfi28IrGAcOK21Iz7d46qZ9/u4I1vgDkwGmrqMmrYvuCJCKynSSjWzWXjNyEc\n' +
+    'CM358wIDAQABAkEAiCddt9Mfn8C3Qd1yNdwduYMGyNMPhdo6mpy+764TfZY7m5Ix\n' +
+    'aAORc8gcxAa4HCgppFZKPMhVHQeQ9PPOyfTNUQIhAP53gQ6dhpoOQJph9EQI3pbw\n' +
+    '+s8Zn2PRuK2qJJGfXHPvAiEA6pdifFp0p0/l2xzhRbWeJTpIU2UpBR++nd8N77od\n' +
+    'Rj0CICWmxwjDlCP9Ud/F+J+MdGr/Ew1LrELXyCyiDTEi1EovAiAtd5XRAD8nxmaI\n' +
+    'ZzqKJj82e+tUroCay6JIOtkao3nVCQIgXhPtwxga9IRKEmPYPPInnjslTW6+OoLA\n' +
+    'LFHMeUwQnvE=\n' +
+    '-----END PRIVATE KEY-----';
 
-    // let encrypted1 = key.encrypt("Message", 'base64');
-    // console.log("RSA = %s",encrypted1);
-
-    // let clientKey = new NodeRSA(RSA_PUBLIC_KEY)
-    // let encrypted = clientKey.encrypt("Message", 'base64')
-    // console.log("RSA = %s",encrypted);
+    const encrypted = crypto.publicEncrypt(RSA_PUBLIC_KEY, new Buffer("test")).toString('hex');
+    console.log("RSA encrypted = %s", encrypted);
+    const decrypted = crypto.privateDecrypt(RSA_PRIVATE_KEY, new Buffer(encrypted, 'hex'));
+    console.log("RSA decrypted = %s", decrypted.toString());
 
   }
 
